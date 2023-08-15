92ece8263635920227e82d4203d5990054b77008
Ckaeiront <stampedesw29@gmail.com>
2023-08-15 20:34:56 -0200

    cardzinho que eu fiz do curso

--- src/index.js
+++ src/index.js
@@ -1,17 +1,61 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const appLogs = {
+  secondImg: "https://i.pinimg.com/564x/64/e8/e7/64e8e7c6abacdd50bc444dde316e5d2d.jpg",
+  textIntro: "Sou um desenvolvedor web e louco por tiozão. Fervendo o leitinho que o tiozão comprou por 3 reais aqui no sitio do vizinho.",
+  imageURL: "https://i.pinimg.com/564x/0a/16/5d/0a165d5e87257feb36ee20b9e5666ae7.jpg",
+};
+
+const SkillList = (props) => {
+  const { skills, emoji, color } = props;
+
+  return (
+    <div className="skill" style={{ backgroundColor: color }}>
+      <span>
+        {skills} {emoji}
+      </span>
+    </div>
+  );
+};
+
+const Intro = (props) => {
+  const { text, name } = props;
+  return (
+    <div>
+      <h1>{name}</h1>
+      <p>{text}</p>
+    </div>
+  );
+};
+
+const Avatar = ({ imageSrc }) => {
+  return <img src={imageSrc} alt="" className="avatar"></img>;
+};
+
+const App = () => {
+  return (
+    <div className="card">
+      <Avatar imageSrc={appLogs.imageURL} />
+      <div className="data">
+        <Intro name="Gustavo Cainavara" text={appLogs.textIntro} />
+        <div className="skill-list">
+          <SkillList skills="HTML & CSS" emoji="👍" color="#2f2f2f" />
+          <SkillList skills="JavaScript" emoji="🖥️" color="#b4aa4e" />
+          <SkillList skills="MongoDB" emoji="📂" color="#72b372" />
+          <SkillList skills="Node" emoji="👨‍💻" color="#3c873a" />
+          <SkillList skills="Mixagem" emoji="🎚️" color="#4caaf2" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
