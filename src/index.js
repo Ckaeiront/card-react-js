49a515befbe15279a6617ef7cd89644e24c33e4d
Ckaeiront <stampedesw29@gmail.com>
2023-08-15 21:18:18 -0200

    ajustes e +1 componente

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 const appLogs = {
-  secondImg: "https://i.pinimg.com/564x/64/e8/e7/64e8e7c6abacdd50bc444dde316e5d2d.jpg",
-  textIntro: "Sou um desenvolvedor web e louco por tiozão. Fervendo o leitinho que o tiozão comprou por 3 reais aqui no sitio do vizinho.",
-  imageURL: "https://i.pinimg.com/564x/0a/16/5d/0a165d5e87257feb36ee20b9e5666ae7.jpg",
+  secondImg:
+    "https://i.pinimg.com/564x/64/e8/e7/64e8e7c6abacdd50bc444dde316e5d2d.jpg",
+  textIntro:
+    "Sou um desenvolvedor web e louco por tiozão. Fervendo o leitinho que o tiozão comprou por 3 reais aqui no sitio do vizinho.",
+  imageURL:
+    "https://i.pinimg.com/564x/0a/16/5d/0a165d5e87257feb36ee20b9e5666ae7.jpg",
 };
 
-const SkillList = (props) => {
+const Skill = (props) => {
   const { skills, emoji, color } = props;
 
   return (
@@ -16,6 +19,18 @@ const SkillList = (props) => {
       <span>
         {skills} {emoji}
       </span>
+    </div>
+  );
+};
+
+const SkillList = () => {
+  return (
+    <div className="skill-list">
+      <Skill skills="HTML & CSS" emoji="👍" color="#2f2f2f" />
+      <Skill skills="JavaScript" emoji="🖥️" color="#b4aa4e" />
+      <Skill skills="MongoDB" emoji="📂" color="#72b372" />
+      <Skill skills="Node" emoji="👨‍💻" color="#3c873a" />
+      <Skill skills="Mixagem" emoji="🎚️" color="#4caaf2" />
     </div>
   );
 };
@@ -40,13 +55,7 @@ const App = () => {
       <Avatar imageSrc={appLogs.imageURL} />
       <div className="data">
         <Intro name="Gustavo Cainavara" text={appLogs.textIntro} />
-        <div className="skill-list">
-          <SkillList skills="HTML & CSS" emoji="👍" color="#2f2f2f" />
-          <SkillList skills="JavaScript" emoji="🖥️" color="#b4aa4e" />
-          <SkillList skills="MongoDB" emoji="📂" color="#72b372" />
-          <SkillList skills="Node" emoji="👨‍💻" color="#3c873a" />
-          <SkillList skills="Mixagem" emoji="🎚️" color="#4caaf2" />
-        </div>
+        <SkillList />
       </div>
     </div>
   );
